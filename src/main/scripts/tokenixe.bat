f8bcbc757dbccf101d262b49214f87c8d8d5b8ff
Jörg Steffen <steffen@dfki.de>
2023-09-14 18:42:24 +0200

    Fix tokenixe scripts

--- src/main/scripts/tokenixe.bat
+++ src/main/scripts/tokenixe.bat
@@ -1,12 +1,12 @@
 @echo off
 
 if NOT "%3"=="" (
-mvn exec:java -Dexec.mainClass=de.dfki.lt.tools.tokenizer.output.XMLOutputter -Dexec.args="%1 %2 %3"
+mvn exec:java -Dexec.mainClass=de.dfki.lt.tools.tokenizer.output.XmlOutputter -Dexec.args="%1 %2 %3"
 exit 0
 )
 
 if NOT "%2"=="" (
-mvn exec:java -Dexec.mainClass=de.dfki.lt.tools.tokenizer.output.XMLOutputter -Dexec.args="%1 %2"
+mvn exec:java -Dexec.mainClass=de.dfki.lt.tools.tokenizer.output.XmlOutputter -Dexec.args="%1 %2"
 exit 0
 )
 
